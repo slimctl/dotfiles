680a04169b953137a5d9e325644f20f4c4159cf3
slimctl <7897182+slimctl@users.noreply.github.com>
2026-01-22 22:10:25 +0100

    feat: update nvim config

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  -- add catppuccin
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
