31a4035271b68ef7e6ee0c3fd48ae8129cdba5f0
slimctl <7897182+slimctl@users.noreply.github.com>
2026-03-31 13:55:24 +0200

    feat(nvim): treesitter cfg

---
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "go",
+      "python",
+      "html",
+      "javascript",
+      "typescript",
+      "yaml",
+      "toml",
+      "gotmpl",
+      "bash",
+      "json",
+      "lua",
+      "css",
+      "tsx",
+      "markdown",
+      "markdown_inline",
+    },
+  },
+}
