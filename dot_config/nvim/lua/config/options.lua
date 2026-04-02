4519cc2e43192673068b3af26c803b75f9f23ec8
slimctl <7897182+slimctl@users.noreply.github.com>
2026-04-02 11:15:00 +0200

    fix(nvim): clipboard yanking with ssh session

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,19 +1,14 @@
 vim.o.clipboard = "unnamedplus"
 
+-- use osc52 over ssh sessions
 if os.getenv("SSH_TTY") then
-  vim.g.clipboard = {
-    name = "OSC 52",
-    copy = {
-      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-    },
-    paste = {
-      ["+"] = function()
-        return vim.fn.getreg("+", 1, true)
-      end,
-      ["*"] = function()
-        return vim.fn.getreg("*", 1, true)
-      end,
-    },
-  }
+  local osc52 = require("vim.ui.clipboard.osc52")
+
+  local function sync_clipboard()
+    local content = vim.v.event.regcontents
+    osc52.copy("+")(content)
+    osc52.copy("*")(content)
+  end
+
+  vim.api.nvim_create_autocmd("TextYankPost", { callback = sync_clipboard() })
 end
